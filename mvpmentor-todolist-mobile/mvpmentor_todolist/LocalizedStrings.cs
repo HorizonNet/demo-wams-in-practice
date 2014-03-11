3a350fae3fd116e2dfc6f25874572d8faf05a9b3
Jan Hentschel <onkelz-hentschel@web.de>
2014-03-11 18:07:45 +0100

    added demo

---
+++ mvpmentor-todolist-mobile/mvpmentor_todolist/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using mvpmentor_todolist.Resources;
+
+namespace mvpmentor_todolist
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
---
+++ mvpmentor-todolist/mvpmentor_todolist/MainPage.xaml.cs
@@ -0,0 +1,188 @@
+﻿using System.Threading.Tasks;
+
+using Microsoft.WindowsAzure.MobileServices;
+using Newtonsoft.Json;
+using System;
+
+using Windows.UI.Popups;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+
+namespace mvpmentor_todolist
+{
+    public class TodoItem
+    {
+        public string Id { get; set; }
+
+        [JsonProperty(PropertyName = "text")]
+        public string Text { get; set; }
+
+        [JsonProperty(PropertyName = "complete")]
+        public bool Complete { get; set; }
+
+        [JsonProperty(PropertyName = "__version")]
+        public string Version { get; set; }
+    }
+
+    public sealed partial class MainPage
+    {
+        private MobileServiceCollection<TodoItem, TodoItem> _items;
+        private readonly IMobileServiceTable<TodoItem> _todoTable = App.MobileService.GetTable<TodoItem>();
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void InsertTodoItem(TodoItem todoItem)
+        {
+            // This code inserts a new TodoItem into the database. When the operation completes
+            // and Mobile Services has assigned an Id, the item is added to the CollectionView
+            try
+            {
+                await _todoTable.InsertAsync(todoItem);
+                _items.Add(todoItem);
+            }
+            catch (MobileServiceInvalidOperationException e)
+            {
+                var errormsg = new MessageDialog(
+                    e.Message,
+                    string.Format("{0} (HTTP {1})", e.Response.ReasonPhrase, (int)e.Response.StatusCode));
+                errormsg.ShowAsync();
+            }
+        }
+
+        private async void RefreshTodoItems()
+        {
+            MobileServiceInvalidOperationException exception = null;
+            try
+            {
+                // This code refreshes the entries in the list view by querying the TodoItems table.
+                // The query excludes completed TodoItems
+                _items = await _todoTable
+                    .Where(todoItem => todoItem.Complete == false)
+                    .ToCollectionAsync();
+            }
+            catch (MobileServiceInvalidOperationException e)
+            {
+                exception = e;
+            }
+
+            if (exception != null)
+            {
+                await new MessageDialog(exception.Message, "Error loading items").ShowAsync();
+            }
+            else
+            {
+                ListItems.ItemsSource = _items;
+            }
+        }
+
+        private async void UpdateCheckedTodoItem(TodoItem item)
+        {
+            // This code takes a freshly completed TodoItem and updates the database. When the MobileService 
+            // responds, the item is removed from the list 
+            await _todoTable.UpdateAsync(item);
+            _items.Remove(item);
+        }
+
+        private void ButtonRefresh_Click(object sender, RoutedEventArgs e)
+        {
+            RefreshTodoItems();
+        }
+
+        private void ButtonSave_Click(object sender, RoutedEventArgs e)
+        {
+            var todoItem = new TodoItem { Text = TextInput.Text };
+            InsertTodoItem(todoItem);
+        }
+
+        private void CheckBoxComplete_Checked(object sender, RoutedEventArgs e)
+        {
+            var cb = (CheckBox)sender;
+            var item = cb.DataContext as TodoItem;
+            UpdateCheckedTodoItem(item);
+        }
+
+        private async void ToDoText_LostFocus(object sender, RoutedEventArgs e)
+        {
+            var tb = (TextBox)sender;
+            var item = tb.DataContext as TodoItem;
+
+            //let's see if the text changed
+            if (item != null && item.Text == tb.Text)
+            {
+                return;
+            }
+
+            if (item == null)
+            {
+                return;
+            }
+
+            item.Text = tb.Text;
+            await UpdateToDoItem(item);
+        }
+
+        private async Task UpdateToDoItem(TodoItem item)
+        {
+            Exception exception = null;
+            try
+            {
+                //update at the remote table
+                await _todoTable.UpdateAsync(item);
+            }
+            catch (MobileServicePreconditionFailedException<TodoItem> writeException)
+            {
+                exception = writeException;
+            }
+            catch (Exception ex)
+            {
+                exception = ex;
+            }
+            if (exception != null)
+            {
+                if (exception is MobileServicePreconditionFailedException)
+                {
+                    //Conflict detected, the item has changed since the last query
+                    //Resolve the conflict between the local and server item
+                    await ResolveConflict(item, ((MobileServicePreconditionFailedException<TodoItem>)exception).Item);
+                }
+                else
+                {
+                    await new MessageDialog(exception.Message, "Update Failed").ShowAsync();
+                }
+            }
+        }
+
+        private async Task ResolveConflict(TodoItem localItem, TodoItem serverItem)
+        {
+            //Ask user to choose the resolution between versions
+            var msgDialog = new MessageDialog(String.Format("Server Text: \"{0}\" \nLocal Text: \"{1}\"\n",
+                                                        serverItem.Text, localItem.Text),
+                                                        "CONFLICT DETECTED - Select a resolution:");
+            var localBtn = new UICommand("Commit Local Text");
+            var serverBtn = new UICommand("Leave Server Text");
+            msgDialog.Commands.Add(localBtn);
+            msgDialog.Commands.Add(serverBtn);
+            localBtn.Invoked = async command =>
+            {
+                // To resolve the conflict, update the version of the 
+                // item being committed. Otherwise, you will keep
+                // catching a MobileServicePreConditionFailedException.
+                localItem.Version = serverItem.Version;
+                // Updating recursively here just in case another 
+                // change happened while the user was making a decision
+                await UpdateToDoItem(localItem);
+            };
+            serverBtn.Invoked = command => RefreshTodoItems();
+            await msgDialog.ShowAsync();
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            RefreshTodoItems();
+        }
+    }
+}
